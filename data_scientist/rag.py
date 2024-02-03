db1e2134168b9c46f97ed3dea1c818147b1f3cbd
mburakbozbey <mburakbozbey@outlook.com>
2024-02-03 23:35:07 +0300

    fix black

--- data_scientist/rag.py
+++ data_scientist/rag.py
@@ -12,18 +12,11 @@ from langchain_community.embeddings import FastEmbedEmbeddings
 
 
 class ChatPDF:
-    """
-    ChatPDF class for question answering.
-    """
-
     vector_store = None
     retriever = None
     chain = None
 
     def __init__(self):
-        """
-        Initialize the ChatPDF class.
-        """
         self.model = ChatOllama(model="mistral")
         self.text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1024, chunk_overlap=100
@@ -32,7 +25,7 @@ class ChatPDF:
             """
             <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context 
             to answer the question. If you don't know the answer, just say that you don't know. Use three sentences
-            maximum and keep the answer concise. [/INST] </s> 
+             maximum and keep the answer concise. [/INST] </s> 
             [INST] Question: {question} 
             Context: {context} 
             Answer: [/INST]
@@ -40,14 +33,11 @@ class ChatPDF:
         )
 
     def ingest(self, pdf_file_path: str):
-        """
-        Ingest a PDF document and process it.
-        """
         docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
 
-        vector_store = Chroma().from_documents(chunks)
+        vector_store = Chroma.from_documents(
             documents=chunks, embedding=FastEmbedEmbeddings()
         )
         self.retriever = vector_store.as_retriever(
@@ -58,23 +48,20 @@ class ChatPDF:
             },
         )
 
-        self.chain = RunnablePassthrough().compose(
-            self.prompt.compose(self.model.compose(StrOutputParser()))
+        self.chain = (
+            {"context": self.retriever, "question": RunnablePassthrough()}
+            | self.prompt
+            | self.model
+            | StrOutputParser()
         )
 
     def ask(self, query: str):
-        """
-        Ask a question and get the answer.
-        """
         if not self.chain:
             return "Please, add a PDF document first."
 
         return self.chain.invoke(query)
 
     def clear(self):
-        """
-        Clear the ChatPDF instance.
-        """
         self.vector_store = None
         self.retriever = None
         self.chain = None
